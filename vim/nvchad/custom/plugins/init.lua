584ee372b4b01034a4f7484d88e32b2131f43822
canta2899 <andreacantarutti@icloud.com>
2022-10-08 20:52:02 +0200

    feat: configuring null-ls

--- vim/nvchad/custom/plugins/init.lua
+++ vim/nvchad/custom/plugins/init.lua
@@ -1,23 +1,36 @@
 local overrides = require "custom.plugins.overrides"
 
 return {
+
   ['folke/which-key.nvim'] = {
     disable = false
   },
+
   ["goolord/alpha-nvim"] = {
     disable = false,
     override_options = overrides.alpha,
   },
+
   ["williamboman/mason.nvim"] = {
     override_options = overrides.mason,
   },
+
+ ["jose-elias-alvarez/null-ls.nvim"] = {
+   after = "nvim-lspconfig",
+   config = function()
+     require "custom.plugins.null-ls"
+   end,
+ },
+
   ["neovim/nvim-lspconfig"] = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.plugins.lspconfig"
     end,
   },
+
   ["nvim-treesitter/nvim-treesitter"] = {
      override_options = overrides.treesitter,
    },
+
 }
