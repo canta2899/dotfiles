214114e7b9b5777381522f25aa781c3e6fa76261
canta2899 <andreacantarutti@icloud.com>
2022-11-03 01:17:18 +0100

    fix: reducing timeouts for nvim+tmux

--- vim/nvchad/custom/init.lua
+++ vim/nvchad/custom/init.lua
@@ -1,5 +1,7 @@
 -- https://github.com/NvChad/NvChad/issues/1417
 vim.opt.relativenumber = true
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 50
 vim.g["loaded_python3_provider"] = nil
 vim.cmd["runtime python3_provider"] = nil
 -- vim.opt.linebreak = true
