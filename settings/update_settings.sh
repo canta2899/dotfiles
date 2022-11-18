7d721bee0bbb864e69ab994ac006000f48571e44
canta2899 <andreacantarutti@icloud.com>
2022-11-18 02:40:45 +0100

    feat: removing useless stuff

--- settings/update_settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR_PATH=$(dirname $(realpath $0))
-
-# -- SETTINGS --------------
-cp $HOME/Library/Application\ Support/Code/User/settings.json $DIR_PATH
-cp -r $HOME/Library/Application\ Support/Code/User/snippets/ $DIR_PATH
-cp $HOME/Library/Application\ Support/Code/User/keybindings.json $DIR_PATH
-
