6fdef7eed78c055f0bc4b325f422f171ec849649
canta2899 <andreacantarutti@icloud.com>
2023-07-29 14:02:38 +0200

    feat: nvim using onedark scheme

--- vim/nvim/lua/canta2899/plugins/color.lua
+++ vim/nvim/lua/canta2899/plugins/color.lua
@@ -1,51 +1,9 @@
 return {
-
-    -----------------
-    -- Tokyonight  --
-    -----------------
-
     {
-        "folke/tokyonight.nvim",
-        priority = 1000, -- make sure to load this before all the other start plugins
-        opts = { style = "moon" },
-        config = function()
-            vim.cmd.colorscheme "tokyonight-storm"
-        end,
+        'navarasu/onedark.nvim',
+        priority = 1000,
+        config = function ()
+            vim.cmd.colorscheme "onedark"
+        end
     }
-
-
-    ---------------
-    -- Material  --
-    ---------------
-
-    -- {
-    --  "marko-cerovac/material.nvim",
-    --   priority = 1000,
-    --   config = function()
-    --     require("material").setup({
-    --       disable = {
-    --         background = false,
-    --       }
-    --     })
-    --     vim.g.material_style = "darker"
-    --     vim.cmd.colorscheme "material"
-    --   end
-    -- }
-
-
-    -----------------
-    -- Rosepine    --
-    -----------------
-
-    -- {
-    --   "rose-pine/neovim",
-    --   name = "rose-pine",
-    --   priority = 1000,
-    --   config = function()
-    --     require("rose-pine").setup({
-    --       variant = "moon",
-    --     })
-    --     vim.cmd.colorscheme "rose-pine"
-    --   end
-    -- }
 }
