d915b8b1b555f9069fadb53c1d6f44f67e32cd7a
canta2899 <andreacantarutti@icloud.com>
2026-04-29 15:30:44 +0200

    removed old reword script

--- scripts/git-reword-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Rewords a git commit in order to change commit mnessage 
-
-REV=$1
-MESSAGE=$2
-FILTER="test $(echo '$GIT_COMMIT') = $(git rev-parse $REV) && echo $MESSAGE || cat"
-git filter-branch --msg-filter "$FILTER" -- --all
