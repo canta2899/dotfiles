2269de5130c969d38c47d7514f7f3b69c4313260
canta2899 <andreacantarutti@icloud.com>
2026-07-16 02:40:08 +0200

    fix: disabled inlay hints in language server

--- vim/lazyvim/lua/plugins/customizations.lua
+++ vim/lazyvim/lua/plugins/customizations.lua
@@ -69,6 +69,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
+      opts.inlay_hints = { enabled = false }
       opts.servers = opts.servers or {}
       opts.servers.marksman = { enabled = false }
       opts.servers.ruff = { mason = false }
